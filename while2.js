fd7b1930e4954b22a57205460fb68de64f58cc93
Tirupathi rao <102864595+tiru94@users.noreply.github.com>
2023-05-24 16:50:32 +0530

    Update while2.js

--- while2.js
+++ while2.js
@@ -3,3 +3,33 @@ while(i<10){
     console.log("Tiru");
     i++;
 }
+
+output:
+
+Tiru
+Tiru
+Tiru
+Tiru
+Tiru
+Tiru
+Tiru
+Tiru
+Tiru
+Tiru
+
+example 2:
+
+var count = 0;
+
+
+while (count < 5) {
+    console.log("hello");
+  count++;
+}
+
+output:
+hello
+hello 
+hello
+hello
+hello
